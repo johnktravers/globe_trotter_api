42df61f130d44af886b7f9c692d023c0437e7766
John Travers <johnk.travers@gmail.com>
2020-02-16 12:57:40 -0700

    Add user, trip, destination, trip destination, and date models
    Co-Authored-By: Zac Isaacson <isaacson.zm@gmail.com>

--- trips/models.py
+++ trips/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
-# Create your models here.
+class User(models.Model):
+  email = models.EmailField(unique=True)
+  name = models.CharField(max_length=100)
+  api_key = models.CharField(max_length=32)
+
+class Trip(models.Model):
+  name = models.CharField(max_length=100)
+  origin = models.CharField(max_length=100)
+  origin_abbrev = models.CharField(max_length=3)
+  origin_lat = models.CharField(max_length=100)
+  origin_long = models.CharField(max_length=100)
+  start_date = models.DateField()
+  end_date = models.DateField()
+  user = models.ForeignKey('User', on_delete=models.CASCADE)
+
+class Destination(models.Model):
+  location = models.CharField(max_length=100)
+  abbrev = models.CharField(max_length=3)
+  lat = models.CharField(max_length=100)
+  long = models.CharField(max_length=100)
+
+class TripDestination(models.Model):
+  trip = models.ForeignKey('Trip', on_delete=models.CASCADE)
+  destination = models.ForeignKey('Destination', on_delete=models.CASCADE)
+
+class Date(models.Model):
+  date = models.DateField()
+  trip_destination = models.ForeignKey('TripDestination', on_delete=models.CASCADE)
