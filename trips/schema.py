c4332597c1fa4a9a73ca9e53c6a20ea2e2ad5d7a
Zac Isaacson <isaacson.zm@gmail.com>
2020-02-21 16:40:39 -0700

    Refactor trip creation using a services file
    Co-Authored-By: John Travers <johnktravers@users.noreply.github.com>

--- trips/schema.py
+++ trips/schema.py
@@ -1,15 +1,10 @@
 import graphene
-import requests
-import environ
-
-env = environ.Env(
-    DEBUG=(bool, False)
-)
+import re
 
 from graphene import ObjectType, Mutation
-
 from graphene_django.types import DjangoObjectType
 from trips.models import User, Trip, Destination, TripDestination, Activity
+from trips.services import get_coordinates, get_airport_code
 
 class UserType(DjangoObjectType):
     class Meta:
@@ -43,22 +38,14 @@ class CreateTrip(Mutation):
     def mutate(self, info, user_api_key, name, origin):
         user = User.objects.get(api_key = user_api_key)
         trip = Trip(name=name, user=user)
-        params = {'address': origin, 'key': env("GEOCODE_API_KEY")}
-        response = requests.get("https://maps.googleapis.com/maps/api/geocode/json", params=params).json()
-        trip.origin = response['results'][0]['formatted_address']
-        coordinates = response['results'][0]['geometry']['location']
-        trip.origin_lat = coordinates['lat']
-        trip.origin_long = coordinates['lng']
-        params = {'term': trip.origin.split(",")[0], 'limit': 1}
-        headers = {'APC-Auth-Secret': env("AIRPORT_CODES_API_SECRET"), 'APC-Auth': env("AIRPORT_CODES_API_KEY")}
-        response = requests.get("https://www.air-port-codes.com/api/v1/multi", params=params, headers=headers).json()
-        # import ipdb; ipdb.set_trace()
-        trip.origin_abbrev = response['airports'][0]['iata']
+        response = get_coordinates(origin)
+        trip.origin = response[0]['formatted_address']
+        trip.origin_lat = response[0]['geometry']['location']['lat']
+        trip.origin_long = response[0]['geometry']['location']['lng']
+        trip.origin_abbrev = get_airport_code(trip.origin)
         trip.save()
 
         return CreateTrip(trip=trip)
-        # return CreateTrip(trip=1, name="Whatever", origin="Denver, CO", origin_lat="12.343", origin_long="123.34", origin_abbrev="DNV", user=user )
-
 
 
 class Query(ObjectType):
