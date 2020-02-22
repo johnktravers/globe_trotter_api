443af9bbb97983a3c84ea3323814de8c815b5a75
John Travers <johnk.travers@gmail.com>
2020-02-22 13:41:46 -0700

    Add createDestination mutation to add a destination to a trip
    Co-Authored-By: Zac Isaacson <isaacson.zm@gmail.com>

--- trips/schema.py
+++ trips/schema.py
@@ -46,6 +46,33 @@ class CreateTrip(Mutation):
 
         return CreateTrip(trip=trip)
 
+class CreateDestination(Mutation):
+    destination = graphene.Field(DestinationType)
+
+    class Arguments:
+        user_api_key = graphene.String(required=True)
+        trip_id = graphene.ID(required=True)
+        location = graphene.String(required= True)
+        start_date = graphene.types.datetime.Date(required=True)
+        end_date = graphene.types.datetime.Date(required=True)
+
+    def mutate(self, info, user_api_key, trip_id, location, start_date, end_date):
+        user = User.objects.get(api_key = user_api_key)
+        trip = Trip.objects.filter(user_id=user.id).get(id=trip_id)
+        destination = Destination()
+        response = get_coordinates(location)
+        destination.location = response[0]['formatted_address']
+        destination.lat = response[0]['geometry']['location']['lat']
+        destination.long = response[0]['geometry']['location']['lng']
+        destination.abbrev = get_airport_code(destination.location)
+        destination.save()
+
+        trip_destination = TripDestination(start_date=start_date, end_date=end_date, trip=trip, destination=destination)
+        trip_destination.save()
+
+        return CreateDestination(destination=destination)
+
+
 
 class Query(ObjectType):
     all_trips = graphene.List(TripType, user_api_key=graphene.String(required=True))
@@ -57,3 +84,4 @@ class Query(ObjectType):
 
 class Mutation(ObjectType):
     create_trip = CreateTrip.Field()
+    create_destination = CreateDestination.Field()
