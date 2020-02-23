2770df961a257247e89ac5ae8e160c6eb786858b
John Travers <johnk.travers@gmail.com>
2020-02-22 17:12:38 -0700

    Add view with yelp_activities endpoint
    Co-Authored-By: Zac Isaacson <isaacson.zm@gmail.com>

---
+++ yelp_activities/views.py
@@ -0,0 +1,29 @@
+from django.shortcuts import render
+from django.http import JsonResponse
+import requests
+import environ
+
+env = environ.Env(
+    DEBUG=(bool, False)
+)
+
+def yelp_activities(request):
+    lat = request.GET['lat']
+    long = request.GET['long']
+    headers = {'Authorization': f'Bearer {env("YELP_API_KEY")}'}
+    params = {'latitude': lat, 'longitude': long, 'categories': 'landmarks', 'sort_by': 'rating'}
+    businesses = requests.get('https://api.yelp.com/v3/businesses/search', params=params, headers=headers).json()['businesses']
+
+    formatted_data = {'activities': []}
+    for business in businesses:
+        formatted_data['activities'].append({
+            'name': business['name'],
+            'address': ', '.join(business['location']['display_address']),
+            'category': business['categories'][0]['title'],
+            'rating': business['rating'],
+            'image': business['image_url'],
+            'lat': business['coordinates']['latitude'],
+            'long': business['coordinates']['longitude']
+        })
+
+    return JsonResponse(formatted_data)
