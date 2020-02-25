165b60dba37b9654ba439ddb21250eea18f89d37
Zac Isaacson <isaacson.zm@gmail.com>
2020-02-25 14:19:37 -0700

    Create test for yelp activities without lat and long
    Co-Authored-By: John Travers <johnktravers@users.noreply.github.com>

--- yelp_activities/tests.py
+++ yelp_activities/tests.py
@@ -6,7 +6,7 @@ class YelpActivitiesTest(unittest.TestCase):
     def setUp(self):
         self.client = Client()
 
-    def test_details(self):
+    def test_successful_activities(self):
         response = self.client.get('/api/v1/yelp_activities/?lat=41.9027835&long=12.4963655')
         data = json.loads(response.content)
 
@@ -19,3 +19,11 @@ class YelpActivitiesTest(unittest.TestCase):
         self.assertTrue(type(data['activities'][0]['rating']) is float)
         self.assertTrue(type(data['activities'][0]['lat']) is float)
         self.assertTrue(type(data['activities'][0]['long']) is float)
+
+    def test_yelp_without_lat_long(self):
+        response = self.client.get('/api/v1/yelp_activities/')
+        data = json.loads(response.content)
+
+        self.assertEqual(response.status_code, 400)
+        self.assertEqual(len(data['errors']), 1)
+        self.assertEqual(data['errors'][0]['message'], "Please specify a location by including lat and long params in the URL.")
