bf3cac4b87be6fa61e7abdc8ba3c015d051a1b85
Zac Isaacson <isaacson.zm@gmail.com>
2020-02-25 14:20:16 -0700

    Add sad path to return 400 error if lat and long not passed into yelp activities
    Co-Authored-By: John Travers <johnktravers@users.noreply.github.com>

--- yelp_activities/views.py
+++ yelp_activities/views.py
@@ -8,22 +8,25 @@ env = environ.Env(
 )
 
 def yelp_activities(request):
-    lat = request.GET['lat']
-    long = request.GET['long']
-    headers = {'Authorization': f'Bearer {env("YELP_API_KEY")}'}
-    params = {'latitude': lat, 'longitude': long, 'categories': 'landmarks', 'sort_by': 'rating'}
-    businesses = requests.get('https://api.yelp.com/v3/businesses/search', params=params, headers=headers).json()['businesses']
+    lat = request.GET.get('lat')
+    long = request.GET.get('long')
+    if lat and long:
+        headers = {'Authorization': f'Bearer {env("YELP_API_KEY")}'}
+        params = {'latitude': lat, 'longitude': long, 'categories': 'landmarks', 'sort_by': 'rating'}
+        businesses = requests.get('https://api.yelp.com/v3/businesses/search', params=params, headers=headers).json()['businesses']
 
-    formatted_data = {'activities': []}
-    for business in businesses:
-        formatted_data['activities'].append({
-            'name': business['name'],
-            'address': ', '.join(business['location']['display_address']),
-            'category': business['categories'][0]['title'],
-            'rating': business['rating'],
-            'image': business['image_url'],
-            'lat': business['coordinates']['latitude'],
-            'long': business['coordinates']['longitude']
-        })
-
-    return JsonResponse(formatted_data)
+        formatted_data = {'activities': []}
+        for business in businesses:
+            formatted_data['activities'].append({
+                'name': business['name'],
+                'address': ', '.join(business['location']['display_address']),
+                'category': business['categories'][0]['title'],
+                'rating': business['rating'],
+                'image': business['image_url'],
+                'lat': business['coordinates']['latitude'],
+                'long': business['coordinates']['longitude']
+            })
+        return JsonResponse(formatted_data)
+    else:
+        errors = {'errors': [{"message": "Please specify a location by including lat and long params in the URL."}]}
+        return JsonResponse(errors, status=400)
